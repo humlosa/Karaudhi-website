21827c3b5c054562390d19ea1875ddad5a55e7a1
humlosa <110449020+humlosa@users.noreply.github.com>
2022-09-21 18:55:43 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>KARAUDHI Database</title>
+</head>
+<body>
+<header class="text-gray-600 body-font" style="user-select: auto;">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center" style="user-select: auto;">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" style="user-select: auto;">
+      <img src="../images/karaudhi_logo.jpg" viewbox="720x600" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewbox="0 0 25 25" style="user-select: auto; height:80px; width:80px" ></img>
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" style="user-select: auto;"></path>
+      </svg>
+      <span class="ml-3 text-xl" color="black" style="user-select: auto;">KARAUDHI</span>
+
+    </a>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center" style="user-select: auto;">
+      <a href="index.php" class="mr-5 hover:text-gray-900" style="user-select: auto;">HOME</a>
+      <a href="about.php" class ="mr-5 hover:text-gray-900" style="user-select: auto;">ABOUT</a>
+      <a href="plants.php" class="mr-5 hover:text-gray-900" style="user-select: auto;">BASIC SEARCH</a>
+      <a href="medicines.php" class="mr-5 hover:text-gray-900" style="user-select: auto;">MEDICINES</a>
+      <a href="#" class="mr-5 hover:text-gray-900" style="user-select: auto;">CONTRIBUTE</a>
+    </nav>
+    </div>
+</header> 
+
+<?php ?>
